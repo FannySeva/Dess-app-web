944c7de775e5bddcf9f704450eab74711aa35dae
Stephanie Serrano <173507@mail.unacar.mx>
2022-05-26 12:03:48 -0500

    JavaScript

---
+++ TareaS.js
@@ -0,0 +1,10 @@
+var Hacer = prompt("Realizaste tus actividades correspondientes? ")
+
+if ( Hacer=="si" || Hacer=="SI") {
+    alert("¡Muy bien!");
+}
+ else if (Hacer=="NO" || Hacer=="no") {
+     alert(" Animo, ha echarle ganas");
+ } else {
+    alert("Tu respuesta no es indentificada");
+ } 
